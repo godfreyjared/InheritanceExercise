f6f237791034ffbcf7654fa689a6024d2f221430
Jared Godfrey <godfreyjared@gmail.com>
2025-08-13 11:33:00 -0600

    Finished project.

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -8,17 +8,17 @@ namespace Inheritance
         {
             // TODO Be sure to follow best practice when creating your classes
 
-            // Create a class Animal
-            // give this class 4 members that all Animals have in common
+            // Create a class Animal: DONE
+            // give this class 4 members that all Animals have in common: DONE
 
 
-            // Create a class Bird
-            // give this class 4 members that are specific to Bird
-            // Set this class to inherit from your Animal Class
+            // Create a class Bird: DONE
+            // give this class 4 members that are specific to Bird: DONE
+            // Set this class to inherit from your Animal Class: DONE
 
-            // Create a class Reptile
-            // give this class 4 members that are specific to Reptile
-            // Set this class to inherit from your Animal Class
+            // Create a class Reptile: DONE
+            // give this class 4 members that are specific to Reptile: DONE
+            // Set this class to inherit from your Animal Class: DONE
 
 
 
@@ -29,11 +29,32 @@ namespace Inheritance
              * Creatively display the class member values 
              */
 
-            /*Create an object of your Reptile class
-             *  give values to your members using the object of your Reptile class
-             *  
-             * Creatively display the class member values 
-             */
+            var myBird = new Bird();
+            myBird.WingColor = "Blue";
+            myBird.CanFly = true;
+            myBird.WillMigrate = true;
+            myBird.BeakLength = 3.5;
+
+            var myLizard = new Reptiles()
+            {
+                IsColdBlooded = true,
+                IsScaly = true,
+                Habitat = "Swamp",
+                RenerateTail = true
+
+            };
+
+            var myAnimals = new Animal[]{myBird, myLizard};
+
+            foreach (var animal in myAnimals)
+            {
+                Console.WriteLine($"Alive:{animal.IsAlive}");
+                Console.WriteLine($"Age:{animal.Age}");
+                Console.WriteLine($"it has:{animal.LegCount} legs.");
+                Console.WriteLine($"It dominantly dwells in:{animal.LandSeaAir}.");
+                Console.WriteLine();
+            }
+            ;
         }
     }
 }
